32f191bf3732ce59ba9c3a0b7b8334758a7808da
zinq7 <87585065+zinq7@users.noreply.github.com>
2023-01-27 17:44:03 -0500

    Rename the file now that the mod is actually a mod

--- ChromaticTrials/ChromaticTrials.cs
+++ ChromaticTrials/ChromaticTrials.cs
@@ -23,7 +23,6 @@ namespace ChromaticTrials
     //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
     public class ChromaticTrials : BaseUnityPlugin
     {
-
         public static Lobby lobby;
         //The Plugin GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config).
         //If we see this PluginGUID as it is on thunderstore, we will deprecate this mod. Change the PluginAuthor and the PluginName !
